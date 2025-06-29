935d4f7500d297460a6f83e19cada55f3e611125
Runjhun saxena <runjhunsaxena6@gmail.com>
2025-06-29 12:40:03 +0530

    Changed logic for conflict resolver

--- sentibaba-backend/src/middleware/auth.middleware.ts
+++ sentibaba-backend/src/middleware/auth.middleware.ts
@@ -1,17 +1,17 @@
-import { Request, Response, NextFunction } from "express";
-import { auth } from "../firebase/firebase";
+// import { Request, Response, NextFunction } from "express";
+// import { auth } from "../firebase/firebase";
 
-export const verifyFirebaseAuth = async (req: Request, res: Response, next: NextFunction) => {
-  const header = req.headers.authorization;
-  if (!header || !header.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Missing auth token" });
-  }
-  const token = header.split(" ")[1];
-  try {
-    const decoded = await auth.verifyIdToken(token);
-    req.user = decoded;                     // Attach user info
-    next();
-  } catch {
-    res.status(401).json({ error: "Invalid token" });
-  }
-};
+// export const verifyFirebaseAuth = async (req: Request, res: Response, next: NextFunction) => {
+//   const header = req.headers.authorization;
+//   if (!header || !header.startsWith("Bearer ")) {
+//     return res.status(401).json({ error: "Missing auth token" });
+//   }
+//   const token = header.split(" ")[1];
+//   try {
+//     const decoded = await auth.verifyIdToken(token);
+//     req.user = decoded;                     // Attach user info
+//     next();
+//   } catch {
+//     res.status(401).json({ error: "Invalid token" });
+//   }
+// };
